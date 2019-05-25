6c74d6332a3074c617652a87e6afd0c2a88fc132
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-25 23:33:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,9 +84,8 @@ def adding_matz
         }
      }
      
- programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
- programmer_hash[:yukihiro_matsumoto][:languages] = "LISP"
- programmer_hash
+ programmer_hash[0] = :yukihiro_matsumoto
+
     
 end
 
